4eb23dc51f7a91cab85328123539503dc1414093
Raphael-dousse <98466908+Raphael-dousse@users.noreply.github.com>
2023-06-13 17:08:40 +0200

    button finished

--- src/js/main.js
+++ src/js/main.js
@@ -13,19 +13,3 @@ function reveal() {
     }
   }
 }
-
-window.addEventListener("scroll", reveal);
-
-const cursorSmall = document.querySelector(".small");
-const cursorBig = document.querySelector(".big");
-
-const positionElement = (e) => {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
-
-  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-
-  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-};
-
-window.addEventListener("mousemove", positionElement);
