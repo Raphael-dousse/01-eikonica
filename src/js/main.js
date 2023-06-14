c88d1efc9a9ce5240fe7175ed575339d79a70352
Raphael-dousse <98466908+Raphael-dousse@users.noreply.github.com>
2023-06-14 09:06:40 +0200

    cursor add

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,14 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+const cursor = document.querySelector(".cursor");
+
+const moveCursor = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+
+  cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
+
+window.addEventListener("mousemove", moveCursor);
